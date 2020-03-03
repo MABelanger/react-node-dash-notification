503bd047a9b102731e566f992c70f218140efa2a
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2020-03-03 10:02:00 -0500

    update

--- server/index.js
+++ server/index.js
@@ -40,9 +40,12 @@ sock.on('message', function(topic, message) {
 })
 
 
-// var raw_script = Buffer.from('4730440220649ea8f12070186a4b4f8e109d3fd5b804aa0d858644ae19c86443db81d510c80220121da5bc6a1cf833a4e5bc36166697cc2908f687e485a7b8d32893250a78254f0121034fec45bcb69c80eadb62d4baa023663647ff946ed814bfc88c7a7441291cf21a', 'hex');
+// var raw_script = Buffer.from('483045022100ad31c224959c2b10ab310207f8a60d653606eb4a7b81fb9aea1919136e23e66102202c8e020f3bdfacf0cd069b22c3dc3711291cdc19f837919dc9c6b8c10eeb766e0121034f17f60f4a15c0511eeed575b861075172460e7af4eb38bb350f6ddd4f809c67', 'hex');
+// var a = new dashcoreJs.Script(raw_script);
+// console.log(a.getPublicKey());
+
 // var s = new dashcoreJs.Script(raw_script);
-// console.log(s.getPublicKey());
+// console.log(s.getPublicKeyHash());
 // 'OP_2 33 0x022df8750480ad5b26950b25c7ba79d3e37d75f640f8e5d9bcd5b150a0f85014da 33 0x03e3818b65bcc73a7d64064106a859cc1a5a728c4345ff0b641209fba0d90de6e9 33 0x021f2f6e1e50cb6a953935c3601284925decd3fd21bc445712576873fb8c6ebc18 OP_3 OP_CHECKMULTISIG'
 
 // s.isPublicKeyHashOut() // false
