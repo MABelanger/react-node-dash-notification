44ad53d107d7ab82f87ddd5e71d9b4e0ad4b9e0d
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2020-03-08 20:27:47 -0400

    add react client

--- client/packages/main/src/lib/InputsOutputsTx/DisplayMessages.js
+++ client/packages/main/src/lib/InputsOutputsTx/DisplayMessages.js
@@ -2,19 +2,34 @@ import React, {useState, useRef, useEffect} from "react";
 
 export function DisplayMessages (props){
 
-  console.log('props.messages', props.messages);
-  
+  const bottomRef = React.useRef(null);
+
+  React.useEffect(() => {
+    // We need to to this for the test in jest
+    if(bottomRef && bottomRef.current && bottomRef.current.scrollIntoView){
+      bottomRef.current.scrollIntoView({ block: 'end', behavior: 'smooth' });
+    }
+  });
+
   if(!props.messages){
     return null;
   }
 
-
   return(
-    <pre>
-      {props.messages.map((message)=>{
-        return JSON.stringify(message, null, 4);
+    <>
+      {props.messages.map((message, i)=>{
+        return (
+          <>
+            <hr/>
+            <h4>tx number: {i}</h4>
+            <pre>
+              {JSON.stringify(message, null, 4)}
+            </pre>
+          </>
+        )
       })}
-    </pre>
+      <div id={'bottomRef'} ref={bottomRef}></div>
+    </>
   );
 
 }
