d2c67678599303e096d895ea486136bd470cf543
Michel-Alexandre Belanger <michel-alexandre.belanger@tritondigital.com>
2020-03-29 12:49:36 -0400

    update

---
+++ dash/cmd_example.sh
@@ -0,0 +1,5 @@
+exe/dashcore-0.15.0/bin/dash-cli -rpcuser=bitcoin -rpcpassword=bitcoin -rpcport=9998 getaddressbalance '{"addresses": ["xyz"]}'
+exe/dashcore-0.15.0/bin/dash-cli -rpcuser=bitcoin -rpcpassword=bitcoin -rpcport=9998 getspentinfo '{"addresses": ["xyz"]}'
+exe/dashcore-0.15.0/bin/dash-cli -rpcuser=bitcoin -rpcpassword=bitcoin -rpcport=9998 getaddresstxids '{"addresses": ["xyz"]}'
+exe/dashcore-0.15.0/bin/dash-cli -rpcuser=bitcoin -rpcpassword=bitcoin -rpcport=9998 getaddressutxos '{"addresses": ["xyz"]}'
+exe/dashcore-0.15.0/bin/dash-cli -rpcuser=bitcoin -rpcpassword=bitcoin -rpcport=9998 getrawtransaction xyz
